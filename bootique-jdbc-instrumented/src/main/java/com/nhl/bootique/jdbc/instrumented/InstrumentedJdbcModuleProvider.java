d0404babe4c3352e1d8f1605d7e9535e9c903193
Andrus Adamchik <andrus@objectstyle.com>
2016-02-19 11:01:37 +0300

    Split metrics-related logic into a separate bootique-jdbc-instrumented     submodule #4
    oops... fixing no override of parent; provider is in the wrong place..
    too bad I don't drink coffee ... I need it

--- bootique-jdbc-instrumented/src/main/java/com/nhl/bootique/jdbc/instrumented/InstrumentedJdbcModuleProvider.java
+++ bootique-jdbc-instrumented/src/main/java/com/nhl/bootique/jdbc/instrumented/InstrumentedJdbcModuleProvider.java
@@ -1,13 +1,22 @@
 package com.nhl.bootique.jdbc.instrumented;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import com.google.inject.Module;
 import com.nhl.bootique.BQModuleProvider;
+import com.nhl.bootique.jdbc.JdbcModule;
 
 public class InstrumentedJdbcModuleProvider implements BQModuleProvider {
 
 	@Override
 	public Module module() {
 		return new InstrumentedJdbcModule();
+	}
+
+	@Override
+	public Collection<Class<? extends Module>> overrides() {
+		return Collections.singleton(JdbcModule.class);
 	}
 
 }

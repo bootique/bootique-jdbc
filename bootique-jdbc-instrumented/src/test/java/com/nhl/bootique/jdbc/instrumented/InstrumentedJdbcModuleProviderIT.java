8d85f735cfbc79a90d90297c812adbad5a27066f
Andrus Adamchik <andrus@objectstyle.com>
2016-03-16 19:43:20 -0400

    refactoring module provider tests per BQ #44
    * side effect - upgrade to BQ 0.15

--- bootique-jdbc-instrumented/src/test/java/com/nhl/bootique/jdbc/instrumented/InstrumentedJdbcModuleProviderIT.java
+++ bootique-jdbc-instrumented/src/test/java/com/nhl/bootique/jdbc/instrumented/InstrumentedJdbcModuleProviderIT.java
@@ -1,20 +1,13 @@
 package com.nhl.bootique.jdbc.instrumented;
 
-import static java.util.stream.Collectors.counting;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
 import org.junit.Test;
 
-import com.nhl.bootique.BQModuleProvider;
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 
 public class InstrumentedJdbcModuleProviderIT {
+
 	@Test
 	public void testPresentInJar() {
-		long c = StreamSupport.stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-				.filter(p -> p instanceof InstrumentedJdbcModuleProvider).collect(counting());
-		assertEquals("No provider found", 1, c);
+		BQModuleProviderChecker.testPresentInJar(InstrumentedJdbcModuleProvider.class);
 	}
 }
--- bootique-jdbc/src/test/java/com/nhl/bootique/jdbc/JdbcModuleProviderIT.java
+++ bootique-jdbc/src/test/java/com/nhl/bootique/jdbc/JdbcModuleProviderIT.java
@@ -1,20 +1,12 @@
 package com.nhl.bootique.jdbc;
 
-import static java.util.stream.Collectors.counting;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
 import org.junit.Test;
 
-import com.nhl.bootique.BQModuleProvider;
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 
 public class JdbcModuleProviderIT {
 	@Test
 	public void testPresentInJar() {
-		long c = StreamSupport.stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-				.filter(p -> p instanceof JdbcModuleProvider).collect(counting());
-		assertEquals("No provider found", 1, c);
+		BQModuleProviderChecker.testPresentInJar(JdbcModuleProvider.class);
 	}
 }

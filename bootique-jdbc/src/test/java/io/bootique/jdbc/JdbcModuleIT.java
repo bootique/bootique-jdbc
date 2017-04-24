d9dfa34c6028d1a0c97e17a922bfc7e808b0f9cb
Andrus Adamchik <andrus@objectstyle.com>
2017-04-24 12:24:44 +0300

    Replacing deprecated calls

--- bootique-jdbc/src/test/java/io/bootique/jdbc/JdbcModuleIT.java
+++ bootique-jdbc/src/test/java/io/bootique/jdbc/JdbcModuleIT.java
@@ -33,9 +33,10 @@ public class JdbcModuleIT {
         BQTestRuntime runtime = testFactory.app("-c", "classpath:dummy-2ds.yml")
                 .autoLoadModules()
                 .module(b -> {
-                    BQCoreModule.contributeVariables(b).addBinding("BQ_JDBC_PARTIAL_PASSWORD").toInstance("p1");
-                    BQCoreModule.contributeVariables(b).addBinding("BQ_JDBC_FULLDS2_PASSWORD").toInstance("p2");
-                    BQCoreModule.contributeVariables(b).addBinding("BQ_JDBC_FULLDSVARS_URL").toInstance("jdbc:dummy");
+                    BQCoreModule.extend(b)
+                            .setVar("BQ_JDBC_PARTIAL_PASSWORD", "p1")
+                            .setVar("BQ_JDBC_FULLDS2_PASSWORD", "p2")
+                            .setVar("BQ_JDBC_FULLDSVARS_URL", "jdbc:dummy");
                 })
                 .createRuntime();
         DataSourceFactory factory = runtime.getRuntime().getInstance(DataSourceFactory.class);

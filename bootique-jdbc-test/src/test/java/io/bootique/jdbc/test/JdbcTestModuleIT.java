82c4f9e850c54a7e57ed6cb2a225dbf6889afbf1
Elena Bondareva <ebondareva@objectstyle.com>
2017-10-27 14:11:13 +0300

    Replace modules overriding by listeners #57: * test jdbc-test extender

---
+++ bootique-jdbc-test/src/test/java/io/bootique/jdbc/test/JdbcTestModuleIT.java
@@ -0,0 +1,161 @@
+package io.bootique.jdbc.test;
+
+import com.google.inject.*;
+import io.bootique.BQRuntime;
+import io.bootique.jdbc.test.runtime.DataSourceListener;
+import io.bootique.log.BootLogger;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class JdbcTestModuleIT {
+
+    @ClassRule
+    public static BQTestFactory TEST_FACTORY = new BQTestFactory();
+
+    @Test
+    public void testListenersClasses_Injected() {
+        BQRuntime runtime = TEST_FACTORY.app("-c", "classpath:io/bootique/jdbc/test/dummy-ds.yml")
+                .autoLoadModules()
+                .module(binder -> {
+                    JdbcTestModule.extend(binder).addDataSourceListener(new TestDataSourceListener1());
+                    JdbcTestModule.extend(binder).addDataSourceListener(new TestDataSourceListener2());
+                }).createRuntime();
+
+
+        TypeLiteral<Set<io.bootique.jdbc.DataSourceListener>> typeLiteral = new TypeLiteral<Set<io.bootique.jdbc.DataSourceListener>>() {
+        };
+
+        Set<io.bootique.jdbc.DataSourceListener> set = runtime.getInstance(Key.get(typeLiteral));
+        assertEquals(set.size(), 3);
+    }
+
+    @Test
+    public void testListeners_Injected() {
+        BQRuntime runtime = TEST_FACTORY.app("-c", "classpath:io/bootique/jdbc/test/dummy-ds.yml")
+                .autoLoadModules()
+                .module(new Module() {
+
+                    @Override
+                    public void configure(Binder binder) {
+                        JdbcTestModule.extend(binder).addDataSourceListener(new TestDataSourceListener1());
+                        JdbcTestModule.extend(binder).addDataSourceListener(new TestDataSourceListener2());
+                    }
+
+                    @Singleton
+                    @Provides
+                    TestDataSourceListener3 provideListener3(BootLogger bootLogger) {
+                        return new TestDataSourceListener3(bootLogger);
+                    }
+
+                    @Singleton
+                    @Provides
+                    TestDataSourceListener4 provideListener4(BootLogger bootLogger) {
+                        return new TestDataSourceListener4(bootLogger);
+                    }
+
+                }).createRuntime();
+
+
+        TypeLiteral<Set<io.bootique.jdbc.DataSourceListener>> typeLiteral = new TypeLiteral<Set<io.bootique.jdbc.DataSourceListener>>() {
+        };
+
+        Set<io.bootique.jdbc.DataSourceListener> set = runtime.getInstance(Key.get(typeLiteral));
+        assertEquals(set.size(), 3);
+    }
+
+    static class TestDataSourceListener1 implements DataSourceListener {
+
+        public TestDataSourceListener1() {
+        }
+
+        @Override
+        public void beforeStartup(String name, String jdbcUrl) {
+
+        }
+
+        @Override
+        public void afterStartup(String name, String jdbcUrl, DataSource dataSource) {
+            System.out.print(name + "after started up!\n");
+        }
+
+        @Override
+        public void afterShutdown(String name, String jdbcUrl) {
+
+        }
+    }
+
+    static class TestDataSourceListener2 implements DataSourceListener {
+
+        public TestDataSourceListener2() {
+        }
+
+        @Override
+        public void beforeStartup(String name, String jdbcUrl) {
+
+        }
+
+        @Override
+        public void afterStartup(String name, String jdbcUrl, DataSource dataSource) {
+            System.out.print(name + "after started up!\n");
+        }
+
+        @Override
+        public void afterShutdown(String name, String jdbcUrl) {
+
+        }
+    }
+
+    static class TestDataSourceListener3 implements DataSourceListener {
+
+        private BootLogger bootLogger;
+
+        public TestDataSourceListener3(BootLogger bootLogger) {
+            this.bootLogger = bootLogger;
+        }
+
+        @Override
+        public void beforeStartup(String name, String jdbcUrl) {
+
+        }
+
+        @Override
+        public void afterStartup(String name, String jdbcUrl, DataSource dataSource) {
+            bootLogger.stdout(name + "after started up!\n");
+        }
+
+        @Override
+        public void afterShutdown(String name, String jdbcUrl) {
+
+        }
+    }
+
+    static class TestDataSourceListener4 implements DataSourceListener {
+
+        private BootLogger bootLogger;
+
+        public TestDataSourceListener4(BootLogger bootLogger) {
+            this.bootLogger = bootLogger;
+        }
+
+        @Override
+        public void beforeStartup(String name, String jdbcUrl) {
+
+        }
+
+        @Override
+        public void afterStartup(String name, String jdbcUrl, DataSource dataSource) {
+            bootLogger.stdout(name + "after started up!\n");
+        }
+
+        @Override
+        public void afterShutdown(String name, String jdbcUrl) {
+
+        }
+    }
+}

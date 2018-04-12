f6b0b9200647c5b948f29daf7b70f8de0a469614
Andrus Adamchik <andrus@objectstyle.com>
2018-04-12 18:14:38 +0300

    Hikari healthchecks are not activated if "jdbc.ds.health" config is absent #80
    * cleanup

--- bootique-jdbc-hikaricp-instrumented/src/main/java/io/bootique/jdbc/instrumented/hikaricp/healthcheck/ConnectivityCheck.java
+++ bootique-jdbc-hikaricp-instrumented/src/main/java/io/bootique/jdbc/instrumented/hikaricp/healthcheck/ConnectivityCheck.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * HikariCP "aliveness" standard check
  */
-public class ConnectivityCheck implements HealthCheck {
+class ConnectivityCheck implements HealthCheck {
 
     private final HikariPoolMXBean pool;
     private final long connectivityCheckTimeout;
@@ -23,30 +23,29 @@ public class ConnectivityCheck implements HealthCheck {
     }
 
     /**
+     * Generates stable qualified name for the {@link ConnectivityCheck}
+     *
+     * @param dataSourceName
+     * @return qualified name bq.jdbc.[dataSourceName].connectivity
+     */
+    static String healthCheckName(String dataSourceName) {
+        return "bq.jdbc." + dataSourceName + ".connectivity";
+    }
+
+    /**
      * The health check obtains a {@link Connection} from the pool and immediately return it.
      * The standard HikariCP internal "aliveness" check will be run.
      *
      * @return {@link HealthCheckOutcome#ok()} if an "alive" {@link Connection} can be obtained,
      * otherwise {@link HealthCheckOutcome#critical()} if the connection fails or times out
-     * @throws Exception {@inheritDoc}
      */
     @Override
-    public HealthCheckOutcome check() throws Exception {
+    public HealthCheckOutcome check() {
 
         try (Connection connection = ((HikariPool) pool).getConnection(connectivityCheckTimeout)) {
             return HealthCheckOutcome.ok();
         } catch (SQLException e) {
             return HealthCheckOutcome.critical(e);
         }
-    }
-
-    /**
-     * Generates stable qualified name for the {@link ConnectivityCheck}
-     *
-     * @param dataSourceName
-     * @return qualified name bq.jdbc.[dataSourceName].connectivity
-     */
-    public static String healthCheckName(String dataSourceName) {
-        return "bq.jdbc." + dataSourceName + ".connectivity";
     }
 }

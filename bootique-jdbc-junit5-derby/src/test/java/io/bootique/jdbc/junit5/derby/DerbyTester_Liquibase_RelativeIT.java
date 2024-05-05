86f88bdec2377d706e7c67bac60f705ee5fe21a4
Andrus Adamchik <andrus@objectstyle.com>
2024-05-05 10:50:31 -0400

    Upgrade to Liquibase 4.x #123
    unit tests for the remaining failures

---
+++ bootique-jdbc-junit5-derby/src/test/java/io/bootique/jdbc/junit5/derby/DerbyTester_Liquibase_RelativeIT.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.bootique.jdbc.junit5.derby;
+
+import io.bootique.BQRuntime;
+import io.bootique.junit5.BQApp;
+import io.bootique.junit5.BQTest;
+import io.bootique.junit5.BQTestTool;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@BQTest
+public class DerbyTester_Liquibase_RelativeIT extends BaseDerbyTesterTest {
+
+    @BQTestTool
+    static final DerbyTester db = DerbyTester
+            .db()
+            .initDBWithLiquibaseChangelog("classpath:io/bootique/jdbc/junit5/derby/DerbyTester_Liquibase_RelativeIT-changelog.yml");
+
+    @BQApp(skipRun = true)
+    static final BQRuntime app = BaseDerbyTesterTest.createRuntime(db);
+
+    @Test
+    @DisplayName("Migrations are run")
+    public void migrationsAreRun() {
+        run(app, c -> {
+            try (Statement s = c.createStatement()) {
+                try (ResultSet rs = s.executeQuery("select * from \"b\"")) {
+                    assertTrue(rs.next());
+                    assertEquals(10, rs.getInt("id"));
+                    assertEquals("myname", rs.getString("name"));
+                }
+            }
+        });
+    }
+}
